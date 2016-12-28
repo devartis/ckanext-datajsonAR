35eb77ec263efa97393f04bd4cc4317f5b4a48ab
Jose A. Salgado <jose.salgado.wrk@gmail.com>
2016-12-28 13:17:21 -0300

    Realizo cambios para que la extension funcione con el portal "Datos.gob.ar" sin el "Andino-Theme"

--- ckanext/datajson/package2pod.py
+++ ckanext/datajson/package2pod.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 try:
     from collections import OrderedDict  # 2.7
 except ImportError:
@@ -16,14 +19,20 @@ class Package2Pod:
 
     seen_identifiers = None
 
-
     @staticmethod
     def wrap_json_catalog(dataset_dict, json_export_map):
-
+        andino_platform = True
         import ckanext.gobar_theme.helpers as gobar_helpers
-        site_title = gobar_helpers.get_theme_config("title.site-title", "")
-        mbox = gobar_helpers.get_theme_config("social.mail", "")
-        site_description = gobar_helpers.get_theme_config("title.site-description", "")
+        try:
+            site_title = gobar_helpers.get_theme_config("title.site-title", "")
+            mbox = gobar_helpers.get_theme_config("social.mail", "")
+            site_description = gobar_helpers.get_theme_config("title.site-description", "")
+        except AttributeError:
+            # Esto significa que no estoy corriendo dentro de Andino.
+            andino_platform = False
+            site_title = ""
+            mbox = ""
+            site_description = ""
         superThemeTaxonomy = "http://datos.gob.ar/superThemeTaxonomy.json"
         import ckan.logic as logic
         from ckan.common import c
@@ -57,6 +66,15 @@ class Package2Pod:
                         mbox = tmp_mbox
                 except Exception:
                     pass
+                if not andino_platform:
+                    try:
+                        site_title = tmp_ckan_config.get('app:main', 'ckan.site.title')
+                    except Exception:
+                        site_title = "No definido en \"config.ini\""
+                    try:
+                        site_description = tmp_ckan_config.get('app:main', 'ckan.site.description')
+                    except Exception:
+                        site_description = "No definido en \"config.ini\""
 
         for theme in logic.get_action('group_list')(context, data_dict_page_results):
             my_themes.append({'id': theme['name'],
@@ -481,7 +499,7 @@ class Wrappers:
                 res_url = res_url.replace('http://[[REDACTED', '[[REDACTED')
                 res_url = res_url.replace('http://http', 'http')
                 if r.get('resource_type') in ['api', 'accessurl']:
-                    resource['accessURL'] = res_url.split('download')[0].replace('/resource/', '/archivo/')
+                    pass  # resource['accessURL'] = res_url
                     if 'mediaType' in resource:
                         resource.pop('mediaType')
                 else:
